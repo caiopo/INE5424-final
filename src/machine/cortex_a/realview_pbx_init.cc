110943cf1afe4644f685b0f601ee57394eeaa236
caiopo <caiopoliveira@gmail.com>
2019-06-03 03:44:27 -0300

    Revert asm setup

--- src/machine/cortex_a/realview_pbx_init.cc
+++ src/machine/cortex_a/realview_pbx_init.cc
@@ -8,12 +8,12 @@ void _startup_cache();
 
 __BEGIN_SYS
 
-void RealView_PBX::pre_init()
-{
+void RealView_PBX::pre_init() {
     _startup_cache();
 }
 
-void RealView_PBX::init() {}
+void RealView_PBX::init() {
+}
 
 __END_SYS
 
